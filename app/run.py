38c9091853c7d9affe8ac70e2ff239186c521363
inseoc <dlstj2717@gmail.com>
2024-01-03 14:48:48 +0900

    230103 main UI modified & TTS option added(dev)

--- app/run.py
+++ app/run.py
@@ -32,6 +32,45 @@ def init_session_state():
         st.session_state["jargon"] = ""
 
 
+def stt_result_ui(uploaded_file=None):
+    '''
+    stt 결과를 텍스트로 제공
+    '''
+    stt_done = False
+
+    files = {"file": uploaded_file}
+    response = requests.post(STT_API_URL, files=files)
+
+    try:
+        st.write(response.json()["result"])
+        st.session_state["stt_source"] = response.json()["result"]
+        stt_done = True
+    except Exception as e:
+        print(e)
+        st.write("Error in file processing.")
+
+    finally:
+        return stt_done
+    
+
+def tts_result_ui():
+    '''
+    tts 결과를 음성과 텍스트로 동시 제공
+    '''
+    response = requests.post(TTS_API_URL)
+
+    try:
+        st.write(response.json()["result"])
+        st.session_state["stt_source"] = response.json()["result"]
+        stt_done = True
+    except Exception as e:
+        print(e)
+        st.write("Error in file processing.")
+
+    finally:
+        return stt_done
+
+
 def input_ui_1():
     '''
     1. free-talking 을 위한 'full_chat' 버전
@@ -63,6 +102,8 @@ def main():
     st.title("○ Setting ○")
 
     init_session_state()
+    
+    stt_done_flag = False
 
     try:
         uploaded_file = st.file_uploader("Upload audio file", type=["mp3", "mp4", "wav"])
@@ -70,26 +111,23 @@ def main():
         uploaded_file = None
         st.write(f"Your file type is not in ['mp3', 'mp4', 'wav'] | Error mag: {e}")
 
-
     if uploaded_file is not None:
-        
-        files = {"file": uploaded_file}
-        response = requests.post(STT_API_URL, files=files)
-
-        try:
-            st.write(response.json()["result"])
-            st.session_state["stt_source"] = response.json()["result"]
-        except Exception as e:
-            print(e)
-            st.write("Error in file processing.")
+        stt_done_flag = stt_result_ui(uploaded_file)
 
     col1, col2 = st.columns(2)
 
     with col1:
         input_ui_1()
 
-    with col2:
-        input_ui_2()
+    if st.session_state["talk_type"] == "role_playing":
+        with col2:
+            input_ui_2()
+
+    st.title("o GPT's Speak o")
+
+    # 24.1.3 개발 이어서..
+    # if stt_done_flag:
+    #     tts_result_ui()
 
 
 if __name__ == "__main__":
